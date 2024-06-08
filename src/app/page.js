31fe36011d946958f47c923f9a3cac7727134aac
danimisme <msubhanr53@gmail.com>
2024-06-08 22:35:02 +0700

    add section 3 to home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Section2 from "@/components/Section/Section2";
 import Section1 from "@/components/Section/Section1";
 import Image from "next/image";
+import Section3 from "@/components/Section/Section3";
 
 export default function Home() {
   return (
     <main>
       <Section1 />
       <Section2 />
+      <Section3 />
       <div className="min-h-screen"></div>
     </main>
   );
