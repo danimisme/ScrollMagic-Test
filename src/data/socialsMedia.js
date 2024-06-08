3a5658da8227450a0257062885def24a3757ef1c
danimisme <msubhanr53@gmail.com>
2024-06-08 21:18:14 +0700

    create data socialsMedia

---
+++ src/data/socialsMedia.js
@@ -0,0 +1,22 @@
+export const socialsMedia = [
+  {
+    icon: "/logo/youtube.svg",
+    link: "https://www.youtube.com/",
+  },
+  {
+    icon: "/logo/instagram.svg",
+    link: "https://www.instagram.com/",
+  },
+  {
+    icon: "/logo/facebook.svg",
+    link: "https://www.facebook.com/",
+  },
+  {
+    icon: "/logo/patreon.svg",
+    link: "https://www.patreon.com/",
+  },
+  {
+    icon: "/logo/telegram.svg",
+    link: "https://www.telegram.com/",
+  },
+];
