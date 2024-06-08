0ae7ef77e22056c24b4f5b9ddadafbeff9beb29e
danimisme <msubhanr53@gmail.com>
2024-06-08 23:05:22 +0700

    add animation to button shelters

--- src/components/Elements/ShelterButton.js
+++ src/components/Elements/ShelterButton.js
@@ -1,8 +1,31 @@
+"use client";
 import Link from "next/link";
-
+import { useEffect } from "react";
+import ScrollMagic from "scrollmagic";
 export default function ShelterButton({ name, id }) {
+  console.log(id);
+  useEffect(() => {
+    const controller = new ScrollMagic.Controller();
+    const shelterButton = document.getElementById(id);
+    new ScrollMagic.Scene({
+      triggerElement: shelterButton,
+      triggerHook: "onEnter",
+      reverse: true,
+      duration: "150%",
+      offset: "-100px",
+    })
+      .setClassToggle(shelterButton, "show")
+      .addTo(controller);
+    return () => {
+      controller.destroy();
+    };
+  }, []);
   return (
-    <Link href="/" className="bg-white  group cursor-default ">
+    <Link
+      id={id}
+      href="/"
+      className="bg-white  group cursor-default hidden-bottom "
+    >
       <div className="flex justify-center items-center h-full py-12 px-6  text-center border-4 border-black group-hover:translate-x-4 group-hover:-translate-y-4 transition duration-300">
         <p className="text-xl">{name}</p>
       </div>
