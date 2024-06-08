128a989b4ce33333f6369873fca1a41bd18a51cc
danimisme <msubhanr53@gmail.com>
2024-06-08 21:37:02 +0700

    create projects data

---
+++ src/data/projectsData.js
@@ -0,0 +1,17 @@
+export const projectsData = [
+  {
+    title: "Emergency Aid. WAR 2022",
+    description:
+      "providing food and medicine to the shelters and animals which lost their homes and families due to the war",
+  },
+  {
+    title: "Non-commercial feed line",
+    description:
+      "construction of industrial production base where food for shelters will be produced on a free basis",
+  },
+  {
+    title: "Education and Control",
+    description:
+      "lectures on communication, organization and coordination of processes, control over the use of aid",
+  },
+];
