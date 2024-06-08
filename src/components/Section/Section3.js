39e3cecd3ff57f14bb4f8ed855875269bfc04172
danimisme <msubhanr53@gmail.com>
2024-06-08 22:33:46 +0700

    create section 3

---
+++ src/components/Section/Section3.js
@@ -0,0 +1,22 @@
+import { sheltersData } from "@/data/sheltersData";
+import ShelterButton from "../Elements/ShelterButton";
+export default function Section3() {
+  return (
+    <div className="bg-[#fcf944]">
+      <div className="container max-w-7xl mx-auto px-10 py-32">
+        <div className="mb-20">
+          <h1 className="text-6xl font-bold mb-10">We Want to help</h1>
+          <p className="text-4xl max-w-6xl leading-[150%]">
+            homeless packs, volunteers, guardians, overstayers, shelters and
+            other organisations
+          </p>
+        </div>
+        <div className="grid grid-cols-3 gap-10">
+          {sheltersData.map((shelter, index) => (
+            <ShelterButton key={index} name={shelter.name} id={shelter.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
